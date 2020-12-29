eec49bcf74d5274fa8d6c62eb528bbfff147a549
Raritoken <raritoken@hotmail.com>
2020-12-29 21:39:01 +0100

    timer pop-up

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,24 @@
-$(function() {
+function Timer(f, t){
+    setTimeout(f, t);
+};
+
+Timer(Help, 5000);
+
+var count = 0;
+$(function buttonclick() {
     $(".aanmelden_button").click(function() {
-        window.open("https://raritoken.github.io/Study-Bot/");
+        count++;
+        if (count >= 2){
+            window.alert("STOP TOUCHING ME!");
+            count = 0;
+        }
     });
 });
+
+function Help(){
+    var txt;
+    if (confirm("Het lijkt erop dat je er niet uit kunt komen, klik op OK voor assistentie.")){
+        txt = "Op dit moment is Study-Bot erg druk bezig, probeer het later opnieuw."; }
+    else{ txt = "Veel succes!"; }
+    window.alert(txt);
+}

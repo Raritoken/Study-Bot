cae8523e811398bc4695debd88437fd51dd2a450
Raritoken <raritoken@hotmail.com>
2021-01-04 17:36:18 +0100

    extra page

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,12 +2,14 @@ function Timer(f, t){
     setTimeout(f, t);
 };
 
-Timer(Help, 5000);
+Timer(Help, 20000);
+Timer(IntroSwitch, 5000);
 
 var count = 0;
 $(function buttonclick() {
     $(".aanmelden_button").click(function() {
         count++;
+        window.open("aanmelden.html", "_self");
         if (count >= 2){
             window.alert("STOP TOUCHING ME!");
             count = 0;
@@ -15,6 +17,29 @@ $(function buttonclick() {
     });
 });
 
+$(function buttonclick() {
+    $(".begrepen_button").click(function() {
+        count++;
+        window.open("home.html", "_self");
+        if (count >= 2){
+            window.alert("STOP TOUCHING ME!");
+            count = 0;
+        }
+    });
+});
+function IntroSwitch(){
+    var txt = "";
+    if (count == 0) {
+        txt = "<br>Klik op \"begrepen\" om verder te gaan.";
+        count++;
+    }
+    else {
+        txt = "Even voorstellen, ik ben Studybod en ik ben je hulp<br><br>en toeverlaat bij het maken van je huiswerk.";
+        count--;
+    }
+    document.getElementById("intro").innerHTML = txt;
+    Timer(IntroSwitch, 5000);
+};
 function Help(){
     var txt;
     if (confirm("Het lijkt erop dat je er niet uit kunt komen, klik op OK voor assistentie.")){
